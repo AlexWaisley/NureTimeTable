b81df669029c9ee8d1424cfd69137c72819b0369
Alex Waisley <alexwaisley@gmail.com>
2024-01-18 13:13:12 +0200

    Update main.ts and App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,6 @@
 import Header from "./components/Header.vue";
 import Main from "./components/Main.vue";
 
-import { createApp } from 'vue'
-import App from './App.vue'
-
-import { createPinia } from 'pinia';
-const pinia = createPinia()
-const app = createApp(App)
-app.use(pinia)
 import { useTableDataStore } from './stores/tableData';
 const tableStore = useTableDataStore();
 
